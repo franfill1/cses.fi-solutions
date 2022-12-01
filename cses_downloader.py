35406c1850a7720000b7de84451449af58e615a4
Francesco Lugli <francilu26@gmail.com>
2022-12-01 15:49:13 +0100

    Update download script

--- cses_downloader.py
+++ cses_downloader.py
@@ -14,17 +14,21 @@ cses_date_format = "%Y-%m-%d %H:%M:%S"
 
 class Submission:
     downloaded: bool = False
-    cses_id: int | None = None
-    task_id: int | None = None
-    task_name: str | None = None
-    sender: int | None = None
-    timestamp: int | None = None
-    language: str | None = None
-    result: str | None = None
-    code: str | None = None
+    cses_id: int
+    status: str | None
+    task_id: int | None
+    task_name: str | None
+    sender: int | None
+    timestamp: int | None
+    language: str | None
+    result: str | None
+    code: str | None
 
-    def __init__(self, cses_id: int):
+    def __init__(self, cses_id: int, timestamp: int | None = None,
+                 status: int | None = None):
         self.cses_id = cses_id
+        self.timestamp = timestamp
+        self.status = status
 
     def download(self):
         url = f"https://cses.fi/problemset/result/{self.cses_id}"
@@ -53,9 +57,12 @@ class Submission:
         code = str(soup.find("pre", class_="prettyprint").string)
         self.code = html.unescape(code)
 
+        self.downloaded = True
         return self
 
     def to_string(self):
+        if not self.downloaded:
+            self.download()
         prefix = "/*\n"
         prefix += f"Task:              {self.task_id} {self.task_name}\n"
         prefix += f"Sender:            {self.sender}\n"
@@ -67,6 +74,75 @@ class Submission:
         prefix += "*/\n"
         body = prefix + self.code
         return body.replace("\r\n", "\n")
+
+
+class Task:
+    downloaded: bool = False
+    cses_id: int | None
+    status: str | None
+    title: str | None
+    category: str | None
+    time_limit: float | None
+    memory_limit: int | None
+    submissions: list[Submission, int, str] | None
+
+    def __init__(self, cses_id: str, title: str | None = None,
+                 status: str | None = None):
+        self.cses_id = cses_id
+        self.status = status
+        self.title = title
+
+    def download(self):
+        url = f"https://cses.fi/problemset/task/{self.cses_id}"
+        soup = BeautifulSoup(s.get(url).text, "html.parser")
+
+        self.title = str(soup.h1.string)
+
+        sidebar = soup.find("div", class_="nav sidebar")
+        self.category = sidebar.contents[1].string
+
+        constraints = soup.find("ul", class_="task-constraints")\
+            .find_all("li")
+        self.time_limit = float(constraints[0].contents[1].split()[0])
+        self.memory_limit = int(constraints[1].contents[1].split()[0])
+
+        subs_href_regex = re.compile(r"^\/problemset\/result\/\d+\/$")
+        submissions_tags = soup.find_all("a", href=subs_href_regex)
+
+        def parse_sub(tag) -> tuple[Submission, int, str]:
+            sub_id = int(tag["href"].split("/")[-2])
+            date_string = str(tag.contents[0]).strip()
+            date = datetime.strptime(date_string, cses_date_format)
+            timestamp = datetime.timestamp(date)
+            result = ""
+            if "full" in tag.span["class"]:
+                result = "ACCEPTED"
+            elif "compile-err" in tag.span["class"]:
+                result = "COMPILATION ERROR"
+            elif "zero" in tag.span["class"]:
+                result = "ZERO"
+            return Submission(sub_id, timestamp, result)
+
+        self.submissions = [parse_sub(tag) for tag in submissions_tags]
+        self.submissions = sorted(self.submissions, key=lambda x: x.timestamp)
+
+        self.downloaded = True
+        return self
+
+    def get_solutions(self):
+        if not self.downloaded:
+            self.download()
+        return [sub for sub in self.submissions if sub.status == "ACCEPTED"]
+
+    def get_last_solution(self):
+        if not self.downloaded:
+            self.download()
+        return self.get_solutions()[-1]
+
+    def get_first_solution(self):
+        if not self.downloaded:
+            self.download()
+        return self.get_solutions()[0]
 
 
 def login(username: str, password: str):
@@ -84,49 +160,35 @@ def login(username: str, password: str):
         raise Exception("Invalid username or password")
 
 
-def get_user_id() -> int:
-    r = s.get("https://cses.fi/")
-    html_doc = r.text
-    soup = BeautifulSoup(html_doc, "html.parser")
-    user_link = soup.find("a", class_="account")["href"]
-    return int(user_link.split("/")[-1])
-
-
-def save_last_sol(task_id: int):
-    url = f"https://cses.fi/problemset/task/{task_id}"
-    r = s.get(url)
-    html_doc = r.text
-    soup = BeautifulSoup(html_doc, "html.parser")
-
-    title = soup.h1.string
-    category = soup.find("div", class_="nav sidebar").contents[1].string
-
-    subs_href_regex = re.compile(r"^\/problemset\/result\/\d+\/$")
-    subs = soup.find_all("a", href=subs_href_regex)
-    sols = [sub for sub in subs if "full" in sub.span["class"]]
-    last_sol = sols[0]
-    last_sol_id = int(last_sol["href"].split("/")[-2])
-    code = Submission(last_sol_id).download().to_string()
-
-    filename = f"{category}/{title}/{title}.cpp"
-    os.makedirs(os.path.split(filename)[0], exist_ok=True)
-    print(f"{category} / {task_id} - {title}")
-    with open(filename, "w") as file:
-        file.write(code)
-
-
-def get_solved_tasks():
+def get_tasks() -> list[Task]:
     r = s.get("https://cses.fi/problemset/")
     html_doc = r.text
     soup = BeautifulSoup(html_doc, "html.parser")
-    tasks = soup.find_all("li", class_="task")
-    solved_tasks = [task for task in tasks
-                    if "full" in task.find("span", class_="icon")["class"]]
-    tasks_ids = [int(task.a["href"].split("/")[-1]) for task in solved_tasks]
-    return tasks_ids
+    tasks_tags = soup.find_all("li", class_="task")
+
+    def parse_task(tag) -> tuple[Task, str, str]:
+        task_id = tag.a["href"].split("/")[-1]
+        title = str(tag.a.string)
+        icon = tag.find("span", class_="icon")
+        status = ""
+        if "full" in icon["class"]:
+            status = "ACCEPTED"
+        elif "zero" in icon["class"]:
+            status = "ZERO"
+        return Task(task_id, title, status)
+
+    tasks = [parse_task(tag) for tag in tasks_tags]
+    return tasks
 
 
 if __name__ == "__main__":
     login(input("Username: "), getpass.getpass())
-    for id in get_solved_tasks():
-        save_last_sol(id)
+    solved_tasks = [task for task in get_tasks() if task.status == "ACCEPTED"]
+    for task in solved_tasks:
+        task.download()
+        code = task.get_last_solution().download().to_string()
+        filename = f"{task.category}/{task.title}/{task.title}.cpp"
+        os.makedirs(os.path.split(filename)[0], exist_ok=True)
+        print(f"{task.category} / {task.cses_id} - {task.title}")
+        with open(filename, "w") as file:
+            file.write(code)
